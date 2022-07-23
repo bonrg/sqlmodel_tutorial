dca75a243c0dde7ba4c9505bfd723a7af88f2e54
bonrg <a.uderbay@kazdream.kz>
2022-07-23 11:45:28 +0600

    impl api

--- src/app/api/hero.py
+++ src/app/api/hero.py
@@ -2,6 +2,7 @@ from fastapi import (
     APIRouter,
     Depends,
     status as st,
+    Query
 )
 from .. import models
 from ..services.hero import HeroService
@@ -16,3 +17,26 @@ router = APIRouter(
 @router.post("/", response_model=models.HeroRead, status_code=st.HTTP_201_CREATED)
 async def create_hero(hero: models.HeroCreate, service: HeroService = Depends()):
     return service.create(hero=hero)
+
+
+@router.get("/", response_model=list[models.HeroRead])
+async def get_heroes(
+        offset: int = 0,
+        limit: int = Query(default=100, lte=100),
+        service: HeroService = Depends()):
+    return service.get_heroes(offset=offset, limit=limit)
+
+
+@router.get("/{hero_id}", response_model=models.HeroRead)
+async def get(hero_id: int, service: HeroService = Depends()):
+    return service.get(hero_id=hero_id)
+
+
+@router.patch("/{hero_id}", response_model=models.HeroRead)
+async def update(hero_id: int, hero: models.HeroUpdate, service: HeroService = Depends()):
+    return service.update(hero_id=hero_id, hero=hero)
+
+
+@router.delete("/{hero_id}")
+async def delete(hero_id: int, service: HeroService = Depends()):
+    return service.delete(hero_id=hero_id)
--- src/app/api/team.py
+++ src/app/api/team.py
@@ -2,6 +2,7 @@ from fastapi import (
     APIRouter,
     Depends,
     status as st,
+    Query
 )
 from .. import models
 from ..services.team import TeamService
@@ -14,5 +15,28 @@ router = APIRouter(
 
 
 @router.post("/", response_model=models.TeamRead, status_code=st.HTTP_201_CREATED)
-async def create_hero(team: models.TeamCreate, service: TeamService = Depends()):
+async def create_team(team: models.TeamCreate, service: TeamService = Depends()):
     return service.create(team=team)
+
+
+@router.get("/", response_model=list[models.TeamRead])
+async def get_teams(
+        offset: int = 0,
+        limit: int = Query(default=100, lte=100),
+        service: TeamService = Depends()):
+    return service.get_teams(offset=offset, limit=limit)
+
+
+@router.get("/{team_id}", response_model=models.TeamRead)
+async def get(team_id: int, service: TeamService = Depends()):
+    return service.get(team_id=team_id)
+
+
+@router.patch("/{team_id}", response_model=models.TeamRead)
+async def update(team_id: int, team: models.TeamUpdate, service: TeamService = Depends()):
+    return service.update(team_id=team_id, team=team)
+
+
+@router.delete("/{team_id}")
+async def delete(team_id: int, service: TeamService = Depends()):
+    return service.delete(team_id=team_id)
